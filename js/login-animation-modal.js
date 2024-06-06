34394fd98c2bf688a7b09d7ebc52c9282b04e243
GonBlank <blanco.gonzalo.v@gmail.com>
2024-06-06 00:23:58 -0300

    Signup modal completed

--- js/login-animation-modal.js
+++ js/login-animation-modal.js
@@ -1,7 +1,7 @@
 //--signup modal--//
 
 var sign_up_modal = document.getElementById("sign-up-modal");
-var sign_up_btn = document.getElementById("sign-up-btn");
+var sign_up_btn = document.getElementById("sign-up-modal-btn");
 var sign_up_span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
