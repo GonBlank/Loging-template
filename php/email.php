97ecadbbb8f554eb822340276d7859e4dedb36f2
GonBlank <blanco.gonzalo.v@gmail.com>
2024-06-06 20:59:17 -0300

    Progress in sign up
    sign up inserts the user correctly into the database and sends the validation email

--- php/email.php
+++ php/email.php
@@ -1,5 +1,4 @@
 <?php
-// Incluir el archivo env.php para obtener las variables de entorno
 require_once 'env.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -10,7 +9,6 @@ require 'PHPmailer/PHPMailer.php';
 require 'PHPmailer/SMTP.php';
 
 
-
 function send_email($body, $subject, $client_email)
 {
 
@@ -18,27 +16,23 @@ function send_email($body, $subject, $client_email)
 
     try {
         //Server settings
-        $mail->SMTPDebug  = 0;                    // Enable verbose debug output
-        $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = SMTP_SERVER;                    // Set the SMTP server to send through
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = SMTP_USERNAME;                     // SMTP username
-        $mail->Password   = SMTP_PASSWORD;                               // SMTP password
-        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
-        $mail->Port       = SMTP_PORT;                                    // TCP port to connect to
+        $mail->SMTPDebug  = 0;                    
+        $mail->isSMTP();                                            
+        $mail->Host       = SMTP_SERVER;                   
+        $mail->SMTPAuth   = true;                                   
+        $mail->Username   = SMTP_USERNAME;                     
+        $mail->Password   = SMTP_PASSWORD;                              
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
+        $mail->Port       = SMTP_PORT;                                    
         //Recipients
         $mail->setFrom(SMTP_USERNAME, 'Elipticnet');
-        $mail->addAddress($client_email); //email del cliente
-        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->addAddress($client_email);
+        $mail->isHTML(true);                                  
         $mail->Subject = $subject;
         $mail->Body    = $body;
 
         $mail->send();
     } catch (Exception $e) {
         echo "[ERROR] EMAIL";
-        //header('Location: index.html?message=email_error');
     }
 }
-
-//echo "[OK] EMAIL";
-//header('Location: index.html?message=email_sent');
--- php/signup.php
+++ php/signup.php
@@ -1,91 +1,101 @@
 <?php
-// Incluir el archivo env.php para obtener las variables de entorno
 require_once 'env.php';
 require_once 'email.php';
 
-try {
-    // Crear conexión usando PDO y variables del archivo env.php
-    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
-    // Establecer el modo de error PDO en excepción
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // Verificar si se recibieron los datos del formulario
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Obtener los valores del formulario
-        $nombre = $_POST['nombre'];
-        $email = $_POST['email'];
-        $raw_password = $_POST['password']; // Contraseña en texto plano
-
-        // Validar la longitud de la contraseña
-        $password_length = strlen($raw_password);
-        if ($password_length < 8 || $password_length > 20) {
-            $conn = null;
-            header('Location: signup.html?result=warn&message=' . urlencode('The password must be between 8 and 20 characters.'));
-            exit; // Detener la ejecución del script si la contraseña no cumple con la longitud requerida
-        }
-
-        // Validar el correo electrónico
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $conn = null;
-            header('Location: signup.html?result=warn&message=' . urlencode('Invalid email format.'));
-            exit; // Detener la ejecución del script si el correo no es válido
-        }
-
-        // Consulta SQL para verificar si el correo ya existe en la tabla "usuarios"
-        $sql_check_email = "SELECT COUNT(*) FROM usuarios WHERE email = :email";
-        $stmt_check_email = $conn->prepare($sql_check_email);
-        $stmt_check_email->bindParam(':email', $email);
-        $stmt_check_email->execute();
-        $email_exists = $stmt_check_email->fetchColumn();
-
-        if ($email_exists) {
-            $conn = null;
-            header('Location: signup.html?result=registered_mail&message=' . urlencode('This email is already registered. Login here !'));
-            exit; // Detener la ejecución del script si el correo ya existe en la base de datos
-        }
-
-        // Generar el hash de la contraseña
-        $hashed_password = password_hash($raw_password, PASSWORD_DEFAULT);
-
-        // Generar un hash aleatorio de 24 caracteres para validar el correo
-        $validation_hash = bin2hex(random_bytes(12)); // 12 bytes = 24 caracteres hexadecimales
-        // Capturar la hora en la que se crea el hash
-        $hash_date = date('Y-m-d H:i:s');
-
-        // Consulta SQL para insertar un nuevo usuario
-        $sql_insert_user = "INSERT INTO usuarios (nombre, email, password, validation_hash, hash_date) VALUES (:nombre, :email, :password, :validation_hash, :hash_date)";
-        $stmt_insert_user = $conn->prepare($sql_insert_user);
-        $stmt_insert_user->bindParam(':nombre', $nombre);
-        $stmt_insert_user->bindParam(':email', $email);
-        $stmt_insert_user->bindParam(':password', $hashed_password);
-        $stmt_insert_user->bindParam(':validation_hash', $validation_hash);
-        $stmt_insert_user->bindParam(':hash_date', $hash_date);
-        $stmt_insert_user->execute();
-
-        // Inserción en la tabla contacts
-        $sql_insert_contact = "INSERT INTO contacts (email) VALUES (:email)";
-        $stmt_insert_contact = $conn->prepare($sql_insert_contact);
-        $stmt_insert_contact->bindParam(':email', $email);
-        $stmt_insert_contact->execute(); // Ejecución de la sentencia preparada
-
-        // Enviar verificación de email
-        $body = "Por favor verifique su email: http://" . DOMAIN . "/LOGIN/validate_email.php?hash=$validation_hash";
-        send_email($body, "Verifique su email", $email);
-
-        // Redirigir después de que se haya ejecutado todo el código correctamente
-        $conn = null;
-        header('Location: signup.html?result=ok&message=' . urlencode('Verify your email to complete registration'));
-        exit;
-    } else {
-        header('Location: signup.html?result=error&message=' . urlencode('Bad request.'));
-        exit;
-    }
-} catch (PDOException $e) {
-    // Cerrar la conexión en caso de error
-    if ($conn) {
-        $conn = null;
-    }
-    header("Location: signup.html?result=error&message=" . urlencode("Sorry, we are experiencing problems creating users, please try again later: " . $e->getMessage()));
+//Validate request method
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $raw_password = $_POST['password'];
+} else {
+    echo 'invalid Request_method';
     exit;
 }
-?>
+
+// Validate password length
+$password_length = strlen($raw_password);
+if ($password_length < 8 || $password_length > 20) {
+    $conn = null;
+    echo "invalid password length";
+    exit;
+}
+
+
+// Validate email
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $conn = null;
+    echo "invalid email";
+    exit;
+}
+
+//connection to the database
+try {
+    $conn = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}catch (PDOException $e) {
+   echo "Database connection failed";
+    $conn = null;
+   
+}
+
+//check if the email already exists in the "users" table
+try {
+    $sql_check_email = "SELECT COUNT(*) FROM users WHERE email = :email";
+    $stmt_check_email = $conn->prepare($sql_check_email);
+    $stmt_check_email->bindParam(':email', $email);
+    $stmt_check_email->execute();
+    $email_exists = $stmt_check_email->fetchColumn();
+
+    if ($email_exists) {
+        $conn = null;
+        echo "The user $email already exist in the data base.";
+        exit; // Detener la ejecución del script si el correo ya existe en la base de datos
+    }
+} catch (PDOException $e) {
+    echo "Error checking for duplicate email: " . $e->getMessage();
+    $conn = null;
+}
+
+
+
+// Generate the password hash
+$hashed_password = password_hash($raw_password, PASSWORD_DEFAULT);
+
+// Generate a random 24-character hash to validate the email
+$validation_hash = bin2hex(random_bytes(12));
+$hash_date = date('Y-m-d H:i:s');
+
+//Insert new user
+try {
+    $sql_insert_user = "INSERT INTO users (name, email, password, validation_hash, hash_date) VALUES (:name, :email, :password, :validation_hash, :hash_date)";
+    $stmt_insert_user = $conn->prepare($sql_insert_user);
+    $stmt_insert_user->bindParam(':name', $name);
+    $stmt_insert_user->bindParam(':email', $email);
+    $stmt_insert_user->bindParam(':password', $hashed_password);
+    $stmt_insert_user->bindParam(':validation_hash', $validation_hash);
+    $stmt_insert_user->bindParam(':hash_date', $hash_date);
+    $stmt_insert_user->execute();
+} catch (PDOException $e) {
+    echo "Error insert user: " . $e->getMessage();
+    $conn = null;
+}
+
+
+try {
+    // Insert contact user
+    $sql_insert_contact = "INSERT INTO contacts (email) VALUES (:email)";
+    $stmt_insert_contact = $conn->prepare($sql_insert_contact);
+    $stmt_insert_contact->bindParam(':email', $email);
+    $stmt_insert_contact->execute(); // Ejecución de la sentencia preparada
+} catch (PDOException $e) {
+    echo "Error insert contact: " . $e->getMessage();
+    $conn = null;
+}
+
+
+// send email verification
+$body = "Please verify your email: http://" . DOMAIN . "/Loging%20template/html/login.html?hash=$validation_hash";
+send_email($body, "Verifique su email", $email);
+$conn = null;
+echo "Created user!";
