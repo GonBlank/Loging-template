8cd407a3896bb8b24baeea994f2d3539a5344d9e
GonBlank <blanco.gonzalo.v@gmail.com>
2024-06-06 16:09:35 -0300

    Modal animation work
    Applied modal disappearing animation

--- js/login-animation-modal.js
+++ js/login-animation-modal.js
@@ -7,13 +7,13 @@ var sign_up_span = document.getElementsByClassName("sign-up-close")[0];
 // When the user clicks the button, open the modal 
 sign_up_btn.onclick = function () {
     sign_up_modal.style.display = "flex";
+    sign_up_modal.classList.remove("deblurring");
+    sign_up_modal.classList.add("blurring");
 }
 
 // When the user clicks on <span> (x), close the modal
 sign_up_span.onclick = function () {
-    closeModalWithAnimation();
-    //sign_up_modal.style.display = "none";
-    
+    closeModalWithAnimation(sign_up_modal);
 }
 
 //--recovery password modal--//
@@ -25,33 +25,34 @@ var recovery_password_span = document.getElementsByClassName("password-recovery-
 // When the user clicks the button, open the modal 
 recovery_password_btn.onclick = function () {
     recovery_password_modal.style.display = "flex";
+    recovery_password_modal.classList.remove("deblurring");
+    recovery_password_modal.classList.add("blurring");
 }
 
 // When the user clicks on <span> (x), close the modal
 recovery_password_span.onclick = function () {
-    recovery_password_modal.style.display = "none";
+    closeModalWithAnimation(recovery_password_modal);
 }
-
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == sign_up_modal) {
-        closeModalWithAnimation()
-        //sign_up_modal.style.display = "none";
+        closeModalWithAnimation(sign_up_modal);
     }
     if (event.target == recovery_password_modal) {
-        recovery_password_modal.style.display = "none";
+        closeModalWithAnimation(recovery_password_modal);
     }
-    
 }
 
-
-
-function closeModalWithAnimation() {
-    var modalContent = document.querySelector(".modal-content");
+function closeModalWithAnimation(modal) {
+    var modalContent = modal.querySelector(".modal-content");
     modalContent.classList.add("disappearing");
+    modal.classList.remove("blurring");
+    modal.classList.add("deblurring");
+
     setTimeout(function() {
-        sign_up_modal.style.display = "none";
+        modal.style.display = "none";
         modalContent.classList.remove("disappearing");
+        modal.classList.remove("deblurring");
     }, 200); // Duración de la animación en milisegundos
 }
