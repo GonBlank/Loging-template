9c00c210b97cdc33794522d9ec53ac09b44be35b
GonBlank <blanco.gonzalo.v@gmail.com>
2024-06-06 15:27:26 -0300

    gitflow-feature-stash: animations-and-styles-correction

--- js/login-animation-modal.js
+++ js/login-animation-modal.js
@@ -1,8 +1,8 @@
 //--signup modal--//
 
 var sign_up_modal = document.getElementById("sign-up-modal");
-var sign_up_btn = document.getElementById("sign-up-modal-btn");
-var sign_up_span = document.getElementsByClassName("close")[0];
+var sign_up_btn = document.getElementById("show-sign-up-modal");
+var sign_up_span = document.getElementsByClassName("sign-up-close")[0];
 
 // When the user clicks the button, open the modal 
 sign_up_btn.onclick = function () {
@@ -11,12 +11,47 @@ sign_up_btn.onclick = function () {
 
 // When the user clicks on <span> (x), close the modal
 sign_up_span.onclick = function () {
-    sign_up_modal.style.display = "none";
+    closeModalWithAnimation();
+    //sign_up_modal.style.display = "none";
+    
 }
+
+//--recovery password modal--//
+
+var recovery_password_modal = document.getElementById("password-recovery-modal");
+var recovery_password_btn = document.getElementById("show-password-recovery-modal");
+var recovery_password_span = document.getElementsByClassName("password-recovery-close")[0];
+
+// When the user clicks the button, open the modal 
+recovery_password_btn.onclick = function () {
+    recovery_password_modal.style.display = "flex";
+}
+
+// When the user clicks on <span> (x), close the modal
+recovery_password_span.onclick = function () {
+    recovery_password_modal.style.display = "none";
+}
+
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
     if (event.target == sign_up_modal) {
-        sign_up_modal.style.display = "none";
+        closeModalWithAnimation()
+        //sign_up_modal.style.display = "none";
     }
+    if (event.target == recovery_password_modal) {
+        recovery_password_modal.style.display = "none";
+    }
+    
+}
+
+
+
+function closeModalWithAnimation() {
+    var modalContent = document.querySelector(".modal-content");
+    modalContent.classList.add("disappearing");
+    setTimeout(function() {
+        sign_up_modal.style.display = "none";
+        modalContent.classList.remove("disappearing");
+    }, 200); // Duración de la animación en milisegundos
 }
