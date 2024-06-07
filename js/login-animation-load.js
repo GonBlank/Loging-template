c975d6e5d9013c343dec1614d55a72c918fc0e44
GonBlank <blanco.gonzalo.v@gmail.com>
2024-06-07 01:33:15 -0300

    Animations and loading button
    The loading button is added and animations are corrected

---
+++ js/login-animation-load.js
@@ -0,0 +1,23 @@
+document.getElementById('login-form').addEventListener('submit', function () {
+    // Ocultar el botón de submit
+    document.getElementById('submit-login-btn').classList.add('d-none');
+    // Mostrar el botón de carga
+    document.getElementById('loading-login-btn').classList.remove('d-none');
+});
+
+document.getElementById('signup-form').addEventListener('submit', function () {
+    // Ocultar el botón de submit
+    document.getElementById('submit-signup-btn').classList.add('d-none');
+    // Mostrar el botón de carga
+    document.getElementById('loading-signup-btn').classList.remove('d-none');
+});
+
+
+
+document.getElementById('password-recovery-form').addEventListener('submit', function () {
+    // Ocultar el botón de submit
+    document.getElementById('submit-password-recovery-btn').classList.add('d-none');
+    // Mostrar el botón de carga
+    document.getElementById('loading-password-recovery-btn').classList.remove('d-none');
+});
+
